7a331e0bdba9596b3960792ee239aa5dd9109733
Surya Aji <suryaajis330@gmail.com>
2024-09-18 08:49:59 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
 
   app.useLogger(logger)
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
